6e5bad32669748dd668358c0e095ca4fa43b7097
jpjanet <jon.paul.janet@gmail.com>
2016-10-02 22:59:20 -0400

    bug fixing

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup,find_packages
 
-setup(name='molSimplify',version="1.0",packages=find_packages(),
+setup(name='molSimplify',version="v1.1-alpha",packages=find_packages(),
       include_package_data = True,
       entry_points={'console_scripts': ['molsimplify = molSimplify.__main__:main']},
       )
