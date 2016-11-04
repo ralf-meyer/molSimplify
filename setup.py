89ef1154256ae0790ad07a7ea910cbc16960bd9e
jpjanet <jon.paul.janet@gmail.com>
2016-11-04 00:24:32 -0400

    same

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup,find_packages
 setup(name='molSimplify',version="v1.1-alpha",packages=find_packages(),
       entry_points={'console_scripts': ['molsimplify = molSimplify.__main__:main']},
       package_data={
-          'molsimplify':["Data/*.dat","Data/*.dat","Bind/*.dat","Ligands/*.dict","icons/*.png"]
+          'molsimplify':["Data/*.dat","Data/*.dat","Bind/*.dat","Ligands/*.dict"]
       },
       include_package_data = True,
      )
