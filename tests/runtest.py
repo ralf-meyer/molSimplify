53704b27c81be3f79c41f97b6ff46fe5b89748ae
Fang Liu <fangliu.ustc@gmail.com>
2017-11-16 20:48:25 -0500

    Temporarily added a file to test the mulitple molsimplify call in one file

---
+++ tests/runtest.py
@@ -0,0 +1,20 @@
+import pytest
+import argparse
+import os
+import openbabel as ob
+import numpy as np
+from molSimplify.Scripts.inparse import *
+from molSimplify.Scripts.generator import *
+from molSimplify.Classes.globalvars import *
+from molSimplify.Classes.mol3D import mol3D
+from molSimplify.Classes.mol3D import distance
+from molSimplify.Classes.atom3D import atom3D
+from pkg_resources import resource_filename, Requirement
+
+infile = resource_filename(Requirement.parse("molSimplify"),"tests/inputs/example_1_noff.in")
+args =['main.py','-i', infile]
+startgen(args,False,False)
+infile = resource_filename(Requirement.parse("molSimplify"),"tests/inputs/example_1.in")
+args =['main.py','-i', infile]
+startgen(args,False,False)
+
