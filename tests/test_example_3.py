dbd5ad58ef8e4ef672db1aa2263f3746ede24583
Demar <dedwards@mit.edu>
2018-01-24 15:40:28 -0500

    added tests based on tutorials

---
+++ tests/test_example_3.py
@@ -0,0 +1,24 @@
+import helperFuncs as hp
+
+def test_example_3(tmpdir):
+    testName="example_3"
+    threshMLBL = 0.1
+    threshLG =  1.0
+    threshOG = 2.0
+    [passNumAtoms,passMLBL,passLG,passOG,pass_report] = hp.runtest(tmpdir,testName,threshMLBL,threshLG,threshOG)
+    assert passNumAtoms
+    assert passMLBL
+    assert passLG
+    assert passOG
+    assert pass_report
+
+def test_example_3_No_FF(tmpdir):
+    testName="example_3"
+    threshMLBL = 0.1
+    threshLG =  1.0
+    threshOG = 2.0
+    [passNumAtoms,passMLBL,passLG,passOG,pass_report] = hp.runtestNoFF(tmpdir,testName,threshMLBL,threshLG,threshOG)
+    assert passMLBL
+    assert passLG
+    assert passOG
+    assert pass_report
---
+++ tests/test_example_7.py
@@ -0,0 +1,24 @@
+import helperFuncs as hp
+
+def test_example_7(tmpdir):
+    testName="example_7"
+    threshMLBL = 0.1
+    threshLG =  1.0
+    threshOG = 3.0 #Increased threshold from 2.0 to 3.0
+    [passNumAtoms,passMLBL,passLG,passOG,pass_report] = hp.runtest(tmpdir,testName,threshMLBL,threshLG,threshOG)
+    assert passNumAtoms
+    assert passMLBL
+    assert passLG
+    assert passOG
+    assert pass_report
+
+def test_example_7_No_FF(tmpdir):
+    testName="example_7"
+    threshMLBL = 0.1
+    threshLG =  1.0
+    threshOG = 3.0
+    [passNumAtoms,passMLBL,passLG,passOG,pass_report] = hp.runtestNoFF(tmpdir,testName,threshMLBL,threshLG,threshOG)
+    assert passMLBL
+    assert passLG
+    assert passOG
+    assert pass_report
