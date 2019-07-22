87de8f60f22035d4463ada62f3a46d4c1719201d
jpjanet <jon.paul.janet@gmail.com>
2019-07-22 11:26:02 -0400

    removed probably-spurious reference to pytest-runner

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(name='molSimplify', version="v1.2.7-alpha", packages=find_packages(),
           'molsimplify': ["Data/*.dat", "Data/*.dat", "Bind/*.dat", "Ligands/*.dict", "icons/*.png"]
       },
       data_files=[("molSimplify", ["molSimplify/Data/ML.dat"])],
-      setup_requires=['pytest-runner'],
       install_requirements=['numpy','scipy','scikit-learn','pandas','keras','tensorflow'],
       tests_require=['pytest'],
       include_package_data=True
