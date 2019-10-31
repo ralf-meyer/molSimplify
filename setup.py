6eb25dbde8c72bf8c4deef3b96ff9a89f70e0ae6
Aditya Nandy <aditya.nandy@gmail.com>
2019-10-30 20:14:52 -0400

    Fix for failed builds

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='molSimplify', version="v1.2.7-alpha", packages=find_packages(),
           'molsimplify': ["Data/*.dat", "Data/*.dat", "Bind/*.dat", "Ligands/*.dict", "icons/*.png"]
       },
       data_files=[("molSimplify", ["molSimplify/Data/ML.dat"])],
-      install_requirements=['numpy','scipy','scikit-learn','pandas','keras','tensorflow==1.14.0'],
+      install_requirements=['numpy','scipy','scikit-learn','pandas','keras','tensorflow'],
       setup_requires=['pytest-runner'],
       tests_require=['pytest'],
       include_package_data=True
