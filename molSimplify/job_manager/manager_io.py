42052c3b6a44b745a079cdfe4790120196020787
harpmit <danielrobertharper@gmail.com>
2020-07-31 22:49:20 -0400

    Add extra protection to to the job manager write_infile() function to ensure it behaves as expected

--- molSimplify/job_manager/manager_io.py
+++ molSimplify/job_manager/manager_io.py
@@ -488,10 +488,11 @@ def write_input(input_dictionary=dict(), name=None, charge=None, spinmult=None,
             infile[prop_name] = prop
 
     if (not infile['charge'] and infile['charge'] != 0) or (not infile['spinmult'] and infile['spinmult'] != 0) or (
-            not infile['name']):
-        print(('Name: ' + infile['name']))
-        print(('Charge: ' + str(infile['charge'])))
-        print(('Spinmult: ' + str(infile['spinmult'])))
+            not infile['name']) or (not infile['coordinates']):
+        print('Name: ' + infile['name'])
+        print('Charge: ' + str(infile['charge']))
+        print('Spinmult: ' + str(infile['spinmult']))
+        print('Coordinates: ' + str(infile['coordinates']))
         raise Exception('Minimum parameters not specified for writing infile')
     if type(infile['charge']) != int or type(infile['spinmult']) != int:
         print(('Charge Type: ' + str(type(infile['charge']))))
