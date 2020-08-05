3a5f892b8938beb2a3c9ddcde04e539fa75515ab
Vyshnavi Vennelakanti <vyshnavi@mit.edu>
2020-08-04 21:19:02 -0400

    changed [from tensorflow import logging] to [from tensorflow.compat.v1 import logging] to get rid of warnings related to the deprecated tensorflow functions

--- molSimplify/__main__.py
+++ molSimplify/__main__.py
@@ -80,7 +80,7 @@ def tensorflow_silence():
     ## thanks to
     # stackoverflow.com/questions/40426502/is-there-a-way-to-suppress-the-messages-tensorflow-prints
     try:
-        from tensorflow import logging
+        from tensorflow.compat.v1 import logging
         logging.set_verbosity(logging.ERROR)
         os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
         os.environ['KMP_WARNINGS'] = '0'
