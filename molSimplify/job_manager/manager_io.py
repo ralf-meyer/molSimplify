17d4ea238a097e82a2a06ce7728df835a173e3e9
harpmit <danielrobertharper@gmail.com>
2020-09-04 10:43:18 -0700

    Reverted a bit too much last time. Fix that discrepancy

--- molSimplify/job_manager/manager_io.py
+++ molSimplify/job_manager/manager_io.py
@@ -497,7 +497,7 @@ def write_input(input_dictionary=dict(), name=None, charge=None, spinmult=None,
             infile[prop_name] = prop
 
     if (not infile['charge'] and infile['charge'] != 0) or (not infile['spinmult'] and infile['spinmult'] != 0) or (
-            not infile['name']):
+            not infile['name']) or (not infile['coordinates']):
         print(('Name: ' + infile['name']))
         print(('Charge: ' + str(infile['charge'])))
         print(('Spinmult: ' + str(infile['spinmult'])))
