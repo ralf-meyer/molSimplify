bab764dac37643b6fb7e92689a407314bf22727d
chenruduan <duanchenru@gmail.com>
2021-03-15 10:04:43 -0400

    add more cd-racs options in get_features in mol3D.

--- molSimplify/Classes/mol3D.py
+++ molSimplify/Classes/mol3D.py
@@ -4976,7 +4976,7 @@ class mol3D:
         }
         return results
 
-    def get_features(self, lac=True, force_generate=False, eq_sym=False, use_dist=False):
+    def get_features(self, lac=True, force_generate=False, eq_sym=False, use_dist=False, NumB=False, Zeff=False, size_normalize=False):
         """Get geo-based RAC features for this complex (if octahedral)
 
         Parameters
@@ -4999,7 +4999,7 @@ class mol3D:
             self.createMolecularGraph()
         geo_type = self.get_geometry_type()
         if geo_type['geometry'] == 'octahedral' or force_generate:
-            names, racs = get_descriptor_vector(self, lacRACs=lac, eq_sym=eq_sym, use_dist=use_dist)
+            names, racs = get_descriptor_vector(self, lacRACs=lac, eq_sym=eq_sym, use_dist=use_dist, NumB=NumB, Zeff=Zeff, size_normalize=size_normalize)
             results = dict(zip(names, racs))
         else:
             print("Warning: Featurization not yet implemented for non-octahedral complexes. Return a empty dict.")
