908ebfb5244929c74b54a5ed92fa2932ff636a01
Freya Edholm <f7mathlover15@gmail.com>
2021-07-12 16:30:07 -0400

    made bonds an attribute

--- molSimplify/Classes/AA3D.py
+++ molSimplify/Classes/AA3D.py
@@ -34,6 +34,8 @@ class AA3D:
         # Peptide bond atoms
         self.c = None
         self.n = None
+        # Bonds
+        self.bonds = {}
             
     def identify(self):
         """ States whether the amino acid is (positively/negatively) charged, polar, or hydrophobic.
@@ -125,30 +127,29 @@ class AA3D:
                 Keyed by atom3D atoms in the amino acid
                 Valued by a set consisting of bonded atoms
         """
-        bonds = {}
         for key in self.atoms:
-            if key not in bonds.keys():
-                bonds[key] = set()
+            if key not in self.bonds.keys():
+                self.bonds[key] = set()
                 if key.greek == 'C':
                     for a in self.atoms:
                         if a.greek == 'O' or a.greek == "CA":
-                            bonds[key].add(a)
-                            bonds[a].add(key)
+                            self.bonds[key].add(a)
+                            self.bonds[a].add(key)
                 if key.greek == 'CA':
                     for a in self.atoms:
                         if a.greek == 'CB' or a.greek == "N":
-                            bonds[key].add(a)
-                            bonds[a].add(key)
+                            self.bonds[key].add(a)
+                            self.bonds[a].add(key)
                 if "G" in key.greek:
                     for a in self.atoms:
                         if 'B' in a.greek or "D" in a.greek:
-                            bonds[key].add(a)
-                            bonds[a].add(key)
+                            self.bonds[key].add(a)
+                            self.bonds[a].add(key)
                 if "E" in key.greek:
                     for a in self.atoms:
                         if 'Z' in a.greek or "D" in a.greek:
-                            bonds[key].add(a)
-                            bonds[a].add(key)
+                            self.bonds[key].add(a)
+                            self.bonds[a].add(key)
         return bonds
 
     def setPeptideAtoms(self):
