7c25272eea9e70bed0b1f6e27f65e77fc2992034
allikeys <allikeys@mit.edu>
2021-07-26 12:37:53 -0400

    new pdb-reader helper fn

--- molSimplify/Classes/helpers.py
+++ molSimplify/Classes/helpers.py
@@ -5,18 +5,26 @@
 #
 #  Dpt of Chemical Engineering, MIT
 
-def pStripSpaces(lst):
-    """ When reading a string from a pdb file, fixes some of the spacing issues
-    that result from strange behavior with whitespace.
+def read_atom(line):
+    labels = ['Type', 'SerialNum', 'Name', 'AltLoc', 'ResName', 'ChainID', 'ResSeq', 'ICode', 'X', 'Y', 'Z', 'Occupancy', 'TempFactor', 'Element', 'Charge']
+    data = [line[0:6].strip(), int(line[6:11]), line[12:16].strip(), line[16:17].strip(), line[17:20].strip(), line[21:22].strip(), int(line[22:26]), line[26:27].strip(), float(line[30:38]), float(line[38:46]), float(line[46:54]), float(line[54:60]), float(line[60:66]), line[70:78].strip(), line[78:80].strip()]
+    atom_dict = dict(zip(labels, data))
+    atom_dict['Element'] = atom_dict['Element'][0] + atom_dict['Element'][1:].lower()
+    return atom_dict
 
-    Parameters
-    ----------
-        lst : a list that has just been split by whitespace from a pdb file
+"""
+def pStripSpaces(l):
+    #When reading a string from a pdb file, fixes some of the spacing issues
+    #that result from strange behavior with whitespace.
 
-    Returns
-    -------
-        good_lst : a list with proper spacing, so it can be read properly
-    """
+    #Parameters
+    #----------
+    #    lst : a list that has just been split by whitespace from a pdb file
+
+    #Returns
+    #-------
+    #    good_lst : a list with proper spacing, so it can be read properly
+
     if '+' in l[-1] or '-' in l[-1]: # fix charge of Sym
         l[-1] = l[-1][:(len(l[-1]) - 2)]
     if '0' in l[-1]: # fix number attached
@@ -81,3 +89,5 @@ def pStripSpaces(lst):
             l = l[:7] + ['-'+y[1], '-'+y[2]] + l[8:]
     if "" in l:
         l.remove("")
+    return l
+"""
