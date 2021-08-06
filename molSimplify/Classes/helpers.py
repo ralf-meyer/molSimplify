ba0033cea420236b9873085382c635006f8ac863
Freya Edholm <f7mathlover15@gmail.com>
2021-08-06 15:48:39 -0400

    fixed syntax

--- molSimplify/Classes/helpers.py
+++ molSimplify/Classes/helpers.py
@@ -62,17 +62,6 @@ def makeMol(a_dict, mols, conf, chains, prev_a_dict, bonds, aa=True):
     loc = a_dict['AltLoc']
     ploc = prev_a_dict["AltLoc"]
     m = 0
-    if loc != '' and ploc != '':
-        l = ord(ploc)
-        if loc > chr(l) and len(mols[(a_dict['ChainID'],
-                                     a_dict['ResSeq'])]) == l-64:
-            if aa:
-                m = AA3D(a_dict['ResName'], a_dict['ChainID'],
-                         a_dict['ResSeq'], a_dict['Occupancy'], loc)
-            else:
-                m = mol3D(a_dict['ResName'], loc)
-            mols[(a_dict['ChainID'], a_dict['ResSeq'])].append(m)
-    prev_a_dict = a_dict
     if (a_dict['ChainID'], a_dict['ResSeq']) not in mols.keys():
         if aa:
             m = AA3D(a_dict['ResName'], a_dict['ChainID'], a_dict['ResSeq'],
@@ -80,6 +69,16 @@ def makeMol(a_dict, mols, conf, chains, prev_a_dict, bonds, aa=True):
         else:
             m = mol3D(a_dict['ResName'], loc)
         mols[(a_dict['ChainID'], a_dict['ResSeq'])] = [m]
+    if loc != '' and ploc != '':
+        l = ord(ploc)
+        if loc > chr(l) and len(mols[(a_dict['ChainID'], a_dict['ResSeq'])]) == l-64:
+            if aa:
+                m = AA3D(a_dict['ResName'], a_dict['ChainID'],
+                         a_dict['ResSeq'], a_dict['Occupancy'], loc)
+            else:
+                m = mol3D(a_dict['ResName'], loc)
+            mols[(a_dict['ChainID'], a_dict['ResSeq'])].append(m)
+    prev_a_dict = a_dict
     if a_dict['ChainID'] not in chains.keys():
         chains[a_dict['ChainID']] = [] # initialize key of chain dictionary
     if m != 0 and int(float(a_dict['Occupancy'])) != 1 and m not in conf:
