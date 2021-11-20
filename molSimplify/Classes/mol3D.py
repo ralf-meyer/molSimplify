c5292efcb4813096e51a13efc8392d6bd31a89ab
gianmarco-terrones <gianmarcogterrones@gmail.com>
2021-11-20 11:12:39 -0500

    Changing all instances of Zeff to Gval. Changing all instances of effective_nuclear_charge to group_number. Changed some docstrings accordingly.

--- molSimplify/Classes/mol3D.py
+++ molSimplify/Classes/mol3D.py
@@ -5204,7 +5204,7 @@ class mol3D:
         return results
 
     def get_features(self, lac=True, force_generate=False, eq_sym=False, 
-                     use_dist=False, NumB=False, Zeff=False, size_normalize=False,
+                     use_dist=False, NumB=False, Gval=False, size_normalize=False,
                      alleq=False, strict_cutoff=False, catom_list=None, MRdiag_dict={}):
         """Get geo-based RAC features for this complex (if octahedral)
 
@@ -5230,7 +5230,7 @@ class mol3D:
         if not force_generate:
             geo_type = self.get_geometry_type()
         if force_generate or geo_type['geometry'] == 'octahedral':
-            names, racs = get_descriptor_vector(self, lacRACs=lac, eq_sym=eq_sym, use_dist=use_dist, NumB=NumB, Zeff=Zeff, 
+            names, racs = get_descriptor_vector(self, lacRACs=lac, eq_sym=eq_sym, use_dist=use_dist, NumB=NumB, Gval=Gval, 
                                                 size_normalize=size_normalize, alleq=alleq, MRdiag_dict=MRdiag_dict)
             results = dict(zip(names, racs))
         else:
