3324e6c2be64ebb8df7db69c5246b231f3df4ba3
Ralf Meyer <meyer.ralf@yahoo.com>
2022-02-07 21:00:02 -0500

    Add molsimplify-optimize cmdl script

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(name='molSimplify',
           'console_scripts': [
               'molsimplify = molSimplify.__main__:main',
               'molscontrol = molSimplify.molscontrol.molscontrol:main',
+<<<<<<< HEAD
               'jobmanager = molSimplify.job_manager.resub:main']
       },
       package_dir={'molSimplify': 'molSimplify'},
@@ -19,6 +20,21 @@ setup(name='molSimplify',
       install_requires=['numpy', 'scipy', 'scikit-learn',
                         'pandas', 'keras', 'tensorflow', 'pyyaml'],
       setup_requires=['pytest-runner'],  # this may result some package conflict in local conda build. comment it out if needed.
+=======
+              'jobmanager = molSimplify.job_manager.resub:main',
+              'molsimplify-optimize = molSimplify.optimize.optimize:main']
+      },
+      package_dir={'molSimplify': 'molSimplify'},
+      package_data={
+          'molSimplify': ["Data/*.dat", "Bind/*.dat", "Ligands/*.dict",
+                          "icons/*.png", "python_nn/*.csv", "python_krr/*.csv",
+                          "tf_nn/*/*", "molscontrol/*/*"]
+      },
+      data_files=[("molSimplify", ["molSimplify/Data/ML.dat"])],
+      install_requires=['numpy', 'scipy', 'scikit-learn', 'pandas', 'keras',
+                        'tensorflow', 'pyyaml', 'ase', 'numdifftools'],
+      setup_requires=['pytest-runner'], # this may result some package conflict in local conda build. comment it out if needed.
+>>>>>>> e11021d (Add molsimplify-optimize cmdl script)
       tests_require=['pytest'],
       include_package_data=True
       )
