02ba945764b11840f9502cf9c1324ae4caa58852
Ralf Meyer <meyer.ralf@yahoo.com>
2022-02-17 15:50:29 -0500

    Initial commit of InternalCoordinates

--- molSimplify/optimize/coordinates.py
+++ molSimplify/optimize/coordinates.py
@@ -12,6 +12,9 @@ class Primitive():
     def derivative(self, xyzs):
         ...
 
+    def diff(self, xyzs1, xyzs2):
+        return self.value(xyzs1) - self.value(xyzs2)
+
 
 class Distance(Primitive):
 
@@ -221,3 +224,62 @@ class Dihedral(Primitive):
                                      - norm_g/b_sq*b)
         dw[3*self.l:3*(self.l+1)] = norm_g/b_sq*b
         return dw
+
+    def diff(self, xyzs1, xyzs2):
+        dw = self.value(xyzs1) - self.value(xyzs2)
+        if dw > np.pi:
+            return dw - 2*np.pi
+        if dw < -np.pi:
+            return dw + 2*np.pi
+        return dw
+
+
+class InternalCoordinates():
+
+    def __init__(self, primitives):
+        self.primitives = primitives
+
+    def B(self, xyzs):
+        B = np.zeros((len(self.primitives), xyzs.size))
+        for i, prim in enumerate(self.primitives):
+            B[i, :] = prim.derivative(xyzs)
+        return B
+
+    def Ginv(self, xyzs):
+        B = self.B(xyzs)
+        return np.linalg.pinv(B @ B.T)
+
+    def Binv(self, xyzs):
+        B = self.B(xyzs)
+        return np.linalg.pinv(B @ B.T) @ B
+
+    def to_internals(self, xyzs):
+        q = np.zeros(len(self.primitives))
+        for i, prim in enumerate(self.primitives):
+            q[i] = prim.value(xyzs)
+        return q
+
+    def to_cartesians(self, dq, xyzs_ref, tol=1e-10, maxstep=0.1, maxiter=50):
+        xyzs = xyzs_ref.copy()
+        for _ in range(maxiter):
+            if np.linalg.norm(dq) < tol:
+                return xyzs
+            step = (self.Binv(xyzs).T @ dq).reshape(xyzs.shape)
+            steplengths = np.sqrt(np.sum(step**2, axis=-1))
+            maxlength = np.max(steplengths)
+            if maxlength > maxstep:
+                step *= maxstep / maxlength
+            # Calculate what this step corresponds to in internals
+            step_q = self.diff_internals(xyzs + step, xyzs)
+            xyzs = xyzs + step
+            # Calculate the step for the next iteration
+            dq -= step_q
+        raise RuntimeError('Transformation to Cartesians not converged within '
+                           f'{maxiter} iterations')
+
+    def diff_internals(self, xyzs1, xyzs2):
+        dq = np.zeros(len(self.primitives))
+        for i, prim in enumerate(self.primitives):
+            dq[i] = prim.diff(xyzs1, xyzs2)
+        return dq
+
---
+++ tests/optimize/test_coordinate_sets.py
@@ -0,0 +1,89 @@
+import os
+import pytest
+import numpy as np
+import ase.build
+import ase.collections
+import geometric.molecule
+import geometric.internal
+from molSimplify.optimize.coordinates import (Distance, Angle,
+                                              LinearAngle, Dihedral,
+                                              InternalCoordinates)
+
+
+@pytest.mark.parametrize('name', ase.collections.g2.names)
+def test_redundant_internals(tmpdir, name):
+    atoms = ase.build.molecule(name)
+    if len(atoms) == 1 or name == 'Si2H6':
+        # Skip single atom systems and Si2H6 because of a 0 != 2 pi error
+        return
+    path = os.path.join(tmpdir, 'tmp.xyz')
+    ase.io.write(path, atoms, plain=True)
+    mol = geometric.molecule.Molecule(path)
+    coords_ref = geometric.internal.PrimitiveInternalCoordinates(
+        mol, connect=True)
+
+    primitives = []
+    for ic in coords_ref.Internals:
+        if isinstance(ic, geometric.internal.Distance):
+            primitives.append(Distance(ic.a, ic.b))
+        elif isinstance(ic, geometric.internal.Angle):
+            primitives.append(Angle(ic.a, ic.b, ic.c))
+        elif isinstance(ic, geometric.internal.Dihedral):
+            primitives.append(Dihedral(ic.a, ic.b, ic.c, ic.d))
+        elif isinstance(ic, geometric.internal.OutOfPlane):
+            primitives.append(Dihedral(ic.a, ic.b, ic.c, ic.d))
+        elif isinstance(ic, geometric.internal.LinearAngle):
+            primitives.append(LinearAngle(ic.a, ic.b, ic.c, ic.axis))
+        else:
+            raise NotImplementedError(f'Internal {type(ic)} not implemented')
+
+    coords = InternalCoordinates(primitives)
+
+    xyzs = atoms.get_positions()
+    # Test transformation to internal coordinates
+    np.testing.assert_allclose(coords.to_internals(xyzs),
+                               coords_ref.calculate(xyzs),
+                               atol=1e-8)
+
+    np.testing.assert_allclose(coords.B(xyzs),
+                               coords_ref.wilsonB(xyzs),
+                               atol=1e-8)
+
+    np.testing.assert_allclose(coords.Ginv(xyzs),
+                               coords_ref.GInverse(xyzs),
+                               atol=1e-8)
+
+    # Test transformation back to cartesian
+    # Try to reconstruct the geometry from a distorted reference
+    np.random.seed(4321)
+    xyzs_dist = xyzs + 0.1*np.random.randn(*xyzs.shape)
+    np.testing.assert_allclose(coords.to_internals(xyzs_dist),
+                               coords_ref.calculate(xyzs_dist),
+                               atol=1e-8)
+
+    dq_ref = coords_ref.calcDiff(xyzs, xyzs_dist)
+    dq = coords.diff_internals(xyzs, xyzs_dist)
+    np.testing.assert_allclose(dq, dq_ref, atol=1e-8)
+    xyzs2_ref = coords_ref.newCartesian(
+        xyzs_dist.flatten(), dq.flatten()).reshape(-1, 3)
+
+    if coords_ref.bork:  # Meaning that the transformation failed:
+        with pytest.raises(RuntimeError):
+            coords.to_cartesians(dq, xyzs_dist, maxstep=np.inf, tol=1e-6)
+        return
+    xyzs2 = coords.to_cartesians(dq, xyzs_dist, maxstep=0.1, tol=1e-6)
+
+    # Build atoms objects for Kabsch algorithm
+    atoms2_ref = atoms.copy()
+    atoms2_ref.set_positions(xyzs2_ref)
+
+    atoms2 = atoms.copy()
+    atoms2.set_positions(xyzs2)
+    # Align using Kabsch algorithm
+    ase.build.minimize_rotation_and_translation(atoms2_ref, atoms2)
+    np.testing.assert_allclose(atoms2.get_positions(),
+                               atoms2_ref.get_positions(), atol=1e-5)
+
+    # Test that final geometry is close to original
+    ase.build.minimize_rotation_and_translation(atoms, atoms2)
+    np.testing.assert_allclose(atoms2.get_positions(), xyzs, atol=1e-5)
