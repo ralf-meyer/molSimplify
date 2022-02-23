fa51c438fbe7ffe5edc41f2d85d6ed06370c5c1f
Ralf Meyer <meyer.ralf@yahoo.com>
2022-02-23 13:53:37 -0500

    Initial commit of schlegel_hessian()

--- molSimplify/optimize/hessians.py
+++ molSimplify/optimize/hessians.py
@@ -7,6 +7,8 @@ import ase.units
 from molSimplify.optimize.calculators import (_xtb_methods,
                                               _openbabel_methods,
                                               get_calculator)
+from molSimplify.optimize.connectivity import (find_connectivity,
+                                               find_primitives)
 
 
 def compute_guess_hessian(atoms, method):
@@ -18,6 +20,86 @@ def compute_guess_hessian(atoms, method):
         H = numerical_hessian(atoms)
         atoms.calc = old_calc
         return H
+
+
+def schlegel_hessian(atoms):
+    """
+    Schlegel, Theoret. Chim. Acta 66, 333-340 (1984).
+    https://doi.org/10.1007/BF00554788
+
+    Parameters
+    ----------
+    atoms : ase.atoms.Atoms
+        Arrangement of atoms.
+    Returns
+    -------
+    H : np.ndarray
+        Guess Hessian in cartesian coordinates and ase units (eV, Ang)
+    """
+    atomic_numbers = atoms.get_atomic_numbers()
+    xyzs = atoms.get_positions()
+    # Calculate the covalent bond distances as they are needed later
+    cov = np.array([ase.data.covalent_radii[num] for num in atomic_numbers])
+    r_cov = cov[:, np.newaxis] + cov[np.newaxis, :]
+    # "Atoms are considered bonded if their internuclear distance is less than
+    # 1.35 times the sum of the covalentradii"
+    bonds = find_connectivity(atoms, threshold=1.35**2, connect_fragments=True)
+    bends, linear_bends, torsions, planars = find_primitives(xyzs, bonds)
+
+    def get_B(num1, num2):
+        """Returns the B parameter given two atomic numbers"""
+        # Sort for simplicity
+        num1, num2 = min(num1, num2), max(num1, num2)
+        if num1 <= 2:  # First period
+            if num2 <= 2:  # first-first
+                return -0.244
+            elif num2 <= 10:  # first-second
+                return 0.352
+            else:  # first-third+
+                return 0.660
+        elif num1 <= 10:  # Second period
+            if num2 <= 10:  # second-second
+                return 1.085
+            else:  # second-third+
+                return 1.522
+        else:  # third+-third+
+            return 2.068
+
+    F_str = []
+    for b in bonds:
+        r = np.linalg.norm(xyzs[b.i] - xyzs[b.k])
+        if r < 1.35 * r_cov[b.i, b.k]:
+            B = get_B(atomic_numbers[b.i], atomic_numbers[b.j])
+            F_str.append(1.734/(r - B)**3)
+        else:
+            # Not covalently bonded atoms (from fragment connection algorithm).
+            # Following geomeTRIC those are assigned a fixed value:
+            F_str.append(0.1 * ase.units.Hartree)
+    F_bend = []
+    for a in bends + linear_bends:
+        if atomic_numbers[a.i] == 1 or atomic_numbers[a.k] == 1:
+            # "either or both terminal atoms hydrogen"
+            F_bend.append(0.160)
+        else:
+            # "all three heavy atom bends"
+            F_bend.append(0.250)
+    F_tors = []
+    for t in torsions:
+        r = np.linalg.norm(xyzs[t.j] - xyzs[t.k])
+        F_tors = 0.0023 - 0.07*(r - r_cov[t.j, t.k])
+    F_oop = []
+    for p in planars:
+        r1 = xyzs[p.j] - xyzs[p.i]
+        r2 = xyzs[p.k] - xyzs[p.i]
+        r3 = xyzs[p.l] - xyzs[p.i]
+        # Additional np.abs() since we do not know the orientation of r1
+        # with respect to r2 x r3.
+        d = 1 - np.abs(np.dot(r1, np.cross(r2, r3)))/(
+            np.linalg.norm(r1)*np.linalg.norm(r2)*np.linalg.norm(r3))
+        F_oop.append(0.045 * d**4)
+
+    H = np.diag(F_str + F_bend + F_tors + F_oop)
+    return H
 
 
 def xtb_hessian(atoms, method):
