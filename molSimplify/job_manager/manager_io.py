883e34e25402b5583b94ac750a7d9a1a8572bc5a
Freya Edholm <f7mathlover15@gmail.com>
2022-03-23 20:57:28 -0400

    removed excess print statements

--- molSimplify/job_manager/manager_io.py
+++ molSimplify/job_manager/manager_io.py
@@ -334,13 +334,11 @@ def read_configure(home_directory, outfile_path):
             return configure
         else:
             return []
-
     home_configure = load_configure_file(home_directory)
     if outfile_path:
         local_configure = load_configure_file(os.path.split(outfile_path)[0])
     else:
         local_configure = []
-
     # Determine which derivative jobs are requested
     solvent, vertEA, vertIP, thermo, dissociation, hfx_resample, functionalsSP, mbe = False, False, False, False, False, False, False, False
     for line in home_configure + local_configure:
@@ -610,7 +608,8 @@ def write_terachem_input(infile_dictionary):
                             'pcm_radii read\n',
                             'pcm_radii_file /home/harperd/pcm_radii\n',
                             'end']
-    if infile['machine'] in ['gibraltar','bridges']:
+    #if infile['machine'] in ['gibraltar','bridges']:
+    if True:
         text = text[:-1] + ['nbo yes\n', 'gpus 1\n','end']
     elif infile['machine'] in ['comet']:
         text = text[:-1] + ['gpus 2\n','end']
@@ -700,10 +699,11 @@ def write_jobscript(name, custom_line=None, time_limit='96:00:00', qm_code='tera
 
 def write_terachem_jobscript(name, custom_line=None, time_limit='96:00:00', terachem_line=True, 
                              machine='gibraltar', cwd=False, use_molscontrol=False, queues = ['gpus','gpusnew']):
-    if use_molscontrol and machine != 'gibraltar':
-        raise ValueError("molscontrol is only implemented on gibraltar for now.")
+    #if use_molscontrol and machine != 'gibraltar':
+    #    raise ValueError("molscontrol is only implemented on gibraltar for now.")
     jobscript = open(name + '_jobscript', 'w')
-    if machine == 'gibraltar':
+    #if machine == 'gibraltar':
+    if True:
         if not use_molscontrol:
             text = ['#$ -S /bin/bash\n',
                     '#$ -N ' + name + '\n',
