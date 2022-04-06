6d4b860cbd63a4239ce7747dce509906a1282103
Ralf Meyer <meyer.ralf@yahoo.com>
2022-04-06 17:34:37 -0400

    Introduce optimizer test case for TMCs

--- tests/optimize/test_optimizers.py
+++ tests/optimize/test_optimizers.py
@@ -11,6 +11,7 @@ from molSimplify.optimize.coordinate_sets import (DelocalizedCoordinates,
                                                   InternalCoordinates)
 from molSimplify.optimize.optimizers import NonCartesianBFGS, NonCartesianLBFGS
 from molSimplify.Scripts.rmsd import kabsch_rmsd
+from pkg_resources import resource_filename, Requirement
 
 
 @pytest.mark.parametrize('opt', [NonCartesianBFGS, NonCartesianLBFGS])
@@ -44,8 +45,12 @@ def test_optimizers_on_H2(opt):
                                       'C6H6', 'butadiene', 'bicyclobutane']
                           for coord_set in ['internal', 'dlc']])
 def test_optimizers_on_organic_molecules(opt, mol, coord_set):
+    # check if openbabel version > 3.0. This is necessary as
+    # OBForceField.GetGradient is not public for prior versions.
+    pytest.importorskip('openbabel', minversion='3.0')
+
     atoms = ase.build.molecule(mol)
-    # slightly distort
+    # slightly distort the molecules
     xyzs = atoms.get_positions()
     rng = np.random.default_rng(1234)
     xyzs += rng.normal(scale=0.05, size=xyzs.shape)
@@ -70,8 +75,53 @@ def test_optimizers_on_organic_molecules(opt, mol, coord_set):
         opt_ref = ase.optimize.LBFGS(atoms_ref)
         opt = NonCartesianLBFGS(atoms, coord_set)
 
-    opt_ref.run(fmax=0.001, steps=300)
-    opt.run(fmax=0.001, steps=300)
+    opt_ref.run(fmax=0.001, steps=100)
+    opt.run(fmax=0.001, steps=100)
+
+    assert kabsch_rmsd(atoms.get_positions(),
+                       atoms_ref.get_positions(),
+                       translate=True) < 1e-2
+
+
+@pytest.mark.parametrize('opt, ligand, coord_set',
+                         [(opt, lig, coord_set)
+                          for opt in ['BFGS', 'LBFGS']
+                          for lig in ['water']
+                          for coord_set in ['internal', 'dlc']])
+def test_optimizers_on_homoleptic_TMCs(opt, ligand, coord_set):
+    """TODO: For now only works on water since UFF does not give reasonable
+    results for the other ligands."""
+    # check if openbabel version > 3.0. This is necessary as
+    # OBForceField.GetGradient is not public for prior versions.
+    pytest.importorskip('openbabel', minversion='3.0')
+
+    xyz_file = resource_filename(
+        Requirement.parse('molSimplify'),
+        f'tests/optimize/inputs/homoleptic_octahedrals/Co_II_{ligand}.xyz')
+    atoms = ase.io.read(xyz_file)
+    xyzs = atoms.get_positions()
+    atoms.calc = OpenbabelFF(ff='UFF')
+
+    # Reference calculation in Cartesian coordinates
+    atoms_ref = atoms.copy()
+    atoms_ref.calc = OpenbabelFF(ff='UFF')
+
+    bonds = find_connectivity(atoms)
+    primitives = get_primitives(xyzs, bonds)
+    if coord_set == 'internal':
+        coord_set = InternalCoordinates(primitives)
+    elif coord_set == 'dlc':
+        coord_set = DelocalizedCoordinates(primitives, xyzs)
+
+    if opt == 'BFGS':
+        opt_ref = ase.optimize.BFGS(atoms_ref)
+        opt = NonCartesianBFGS(atoms, coord_set)
+    elif opt == 'LBFGS':
+        opt_ref = ase.optimize.LBFGS(atoms_ref)
+        opt = NonCartesianLBFGS(atoms, coord_set)
+
+    opt_ref.run(fmax=0.001, steps=100)
+    opt.run(fmax=0.001, steps=100)
 
     assert kabsch_rmsd(atoms.get_positions(),
                        atoms_ref.get_positions(),
