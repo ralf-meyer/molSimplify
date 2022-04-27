8f856964f3559fb9a5815614da2581f9fe96fb18
Gianmarco <gianmarcogterrones@gmail.com>
2022-04-27 13:21:37 -0400

    changed versions in mols.yml and setup.py for the biannual conda release of molSimplify

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='molSimplify',
-      version='v1.6.0',
+      version='v1.7.0',
       packages=find_packages(),
       entry_points={
           'console_scripts': [
