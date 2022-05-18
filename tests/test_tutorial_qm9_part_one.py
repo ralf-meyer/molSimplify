4591d67f468aa8b4021e74a486b4364ac39edd1f
vyshnavivennelakanti <vyshnavi@mit.edu>
2022-05-17 23:44:38 -0400

    edited qm9 part one python file

--- tests/test_tutorial_qm9_part_one.py
+++ tests/test_tutorial_qm9_part_one.py
@@ -1,7 +1,7 @@
 import helperFuncs as hp
 
 
-def test_tutorial_qm9_part_one(tmpdir):
+def test_tutorial_qm9_part_one():
     testName = "tutorial_qm9_part_one"
-    hp.runtest_num_atoms_in_xyz(tmpdir, testName)
+    hp.runtest_num_atoms_in_xyz(testName)
 
