2fe7b2b3e174c0162e317c4ba20e5ee098234ff3
vyshnavivennelakanti <vyshnavi@mit.edu>
2022-05-17 23:26:14 -0400

    edited qm9 part one python file

--- tests/test_tutorial_qm9_part_one.py
+++ tests/test_tutorial_qm9_part_one.py
@@ -1,8 +1,7 @@
 import helperFuncs as hp
 
 
-def test_tutorial_qm9_part_one(xyzfile):
+def test_tutorial_qm9_part_one(tmpdir):
     testName = "tutorial_qm9_part_one"
-    threshOG = 2.0
-    hp.runtest_num_atoms_in_xyz(xyzfile)
+    hp.runtest_num_atoms_in_xyz(tmpdir, testName)
 
