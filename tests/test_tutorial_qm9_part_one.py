82f9b1cd1506a90391321ee9cc34a8a041098863
dbkchu <46388253+dbkchu@users.noreply.github.com>
2022-05-18 15:09:55 -0400

    fix qm9 test
    change directory to include tests/ and remove skip statement

--- tests/test_tutorial_qm9_part_one.py
+++ tests/test_tutorial_qm9_part_one.py
@@ -2,7 +2,6 @@ import pytest
 import helperFuncs as hp
 
 
-@pytest.mark.skip
 def test_tutorial_qm9_part_one(tmpdir):
     testName = "tutorial_qm9_part_one"
     hp.runtest_num_atoms_in_xyz(tmpdir, testName)
